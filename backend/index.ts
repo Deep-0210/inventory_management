5748b891a7d6bf6ac12f120fa8bed8c45879b642
DEEP-5772 <deep485386@gmail.com>
2025-06-29 17:19:35 +0530

    Add redis for caching login user data

--- backend/index.ts
+++ backend/index.ts
@@ -13,35 +13,41 @@ const PORT = process.env.Server_Port || 3000;
 const DB_URL = `${process.env.Database_Connection}/inventory`;
 
 // Rate limiter middleware
-const globalLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 100,
-  message: 'Too many requests from this IP, try again in a minute.',
-});
+// const globalLimiter = rateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   max: 100,
+//   message: 'Too many requests from this IP, try again in a minute.',
+// });
 
 const numCPUs = os.cpus().length;
 
 if (cluster.isPrimary) {
-  console.log(`Primary process started: PID ${process.pid}`);
-  // Fork workers based on number of CPU cores
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+    console.log(`Primary process started: PID ${process.pid}`);
+    // Fork workers based on number of CPU cores
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 } else {
-  console.log(`Worker process started: PID ${process.pid}`);
+    console.log(`Worker process started: PID ${process.pid}`);
 
-  const app = express();
-  app.use(express.json());
-  app.use(cors());
-  app.use(globalLimiter);
-  app.use('/', route);
+    import('./redisClient').then(async ({ default: redisClient }) => {
+        await redisClient.connect();
 
-  mongoose
-    .connect(DB_URL, { autoIndex: true })
-    .then(() => console.log(`Database connected by PID ${process.pid}`))
-    .catch((err) => console.error('Database connection error:', err));
+        const app = express();
+        app.use(express.json());
+        app.use(cors());
+        //   app.use(globalLimiter);
+        app.use('/', route);
 
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT} [PID ${process.pid}]`);
-  });
+        mongoose
+            .connect(DB_URL, { autoIndex: true })
+            .then(() => console.log(`Database connected by PID ${process.pid}`))
+            .catch((err) => console.error('Database connection error:', err));
+
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT} [PID ${process.pid}]`);
+        });
+    }).catch((err) => {
+        console.error('Failed to connect to Redis:', err);
+    })
 }
